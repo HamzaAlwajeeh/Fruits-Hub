6c1cfdadaf9984f6d0cc5ca1f96883df5e41bc98
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-16 06:03:16 +0300

    Refactor SplashViewBody to StatefulWidget and implement navigation to OnBoardingView after a delay; update route generation for OnBoardingView.

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fruits_hub/Features/on_Boarding/presentation/views/on_boarding_view.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    navigatorToOnBoardingView();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,5 +31,10 @@ class SplashViewBody extends StatelessWidget {
         SvgPicture.asset(Assets.imagesSplashBottom, fit: BoxFit.fill),
       ],
     );
+  }
+
+  void navigatorToOnBoardingView() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
   }
 }
