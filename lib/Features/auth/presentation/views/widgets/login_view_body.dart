6ad60e29d2b58b0a0485a3b035a2704e6db37799
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-17 14:05:32 +0300

    Enhance login view with UI components and styling

--- lib/Features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/Features/auth/presentation/views/widgets/login_view_body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:fruits_hub/Features/auth/presentation/views/widgets/custom_text_form_field.dart';
 import 'package:fruits_hub/core/constants/constants.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:fruits_hub/core/widgets/custom_button.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
@@ -12,11 +14,14 @@ class LoginViewBody extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(kDefaultPadding),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(height: 24),
             CustomTextFormField(
               textInputType: TextInputType.emailAddress,
               hintText: 'البريد الإلكتروني',
             ),
+            SizedBox(height: 16),
             CustomTextFormField(
               textInputType: TextInputType.visiblePassword,
               hintText: 'كلمة المرور',
@@ -24,6 +29,33 @@ class LoginViewBody extends StatelessWidget {
                 Icons.remove_red_eye,
                 color: AppColors.textgreyColor,
               ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'نسيت كلمة المرور؟',
+              style: TextStyles.semiBold14.copyWith(
+                color: AppColors.lightprimaryColor,
+              ),
+            ),
+            SizedBox(height: 33),
+            CustomButton(onPressed: () {}, text: 'تسجيل الدخول'),
+            SizedBox(height: 33),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'لا تمتلك حساب؟ ',
+                  style: TextStyles.semiBold16.copyWith(
+                    color: AppColors.textgreyColor,
+                  ),
+                ),
+                Text(
+                  'قم بإنشاء حساب',
+                  style: TextStyles.semiBold16.copyWith(
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
