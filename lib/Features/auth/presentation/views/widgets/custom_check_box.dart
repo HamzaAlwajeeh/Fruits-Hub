52125f101de3524aea4e2b371e4ea8699cdd1902
HamzaAlwajeeh <ha20mza60@gmail.com>
2025-09-18 08:56:09 +0300

    Refactor checkbox to be stateless with callback

--- lib/Features/auth/presentation/views/widgets/custom_check_box.dart
+++ lib/Features/auth/presentation/views/widgets/custom_check_box.dart
@@ -3,23 +3,21 @@ import 'package:flutter_svg/svg.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
 
-class CustomCheckBox extends StatefulWidget {
-  const CustomCheckBox({super.key});
-
-  @override
-  State<CustomCheckBox> createState() => _CustomCheckBoxState();
-}
-
-class _CustomCheckBoxState extends State<CustomCheckBox> {
-  bool isChecked = false;
+class CustomCheckBox extends StatelessWidget {
+  const CustomCheckBox({
+    super.key,
+    required this.isChecked,
+    required this.onChanged,
+  });
+  final bool isChecked;
+  final ValueChanged<bool> onChanged;
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
       duration: Duration(microseconds: 500),
       child: GestureDetector(
         onTap: () {
-          isChecked = !isChecked;
-          setState(() {});
+          onChanged(!isChecked);
         },
         child: Container(
           width: 24,
@@ -38,7 +36,7 @@ class _CustomCheckBoxState extends State<CustomCheckBox> {
                     padding: const EdgeInsets.all(2),
                     child: SvgPicture.asset(Assets.imagesCheckIcon),
                   )
-                  : null,
+                  : const SizedBox(),
         ),
       ),
     );
--- lib/Features/auth/presentation/views/widgets/custom_tearms_and_conditions.dart
+++ lib/Features/auth/presentation/views/widgets/custom_tearms_and_conditions.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/Features/auth/presentation/views/widgets/custom_check_box.dart';
 
-class CustomTearmsAndConditions extends StatelessWidget {
+class CustomTearmsAndConditions extends StatefulWidget {
   const CustomTearmsAndConditions({super.key});
 
+  @override
+  State<CustomTearmsAndConditions> createState() =>
+      _CustomTearmsAndConditionsState();
+}
+
+class _CustomTearmsAndConditionsState extends State<CustomTearmsAndConditions> {
+  bool isAcceptTearms = false;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        CustomCheckBox(),
+        CustomCheckBox(
+          onChanged: (value) {
+            isAcceptTearms = value;
+            setState(() {});
+          },
+          isChecked: isAcceptTearms,
+        ),
         const SizedBox(width: 16),
         // Row(
         //   children: [
